59a31da3ff7b3546dd60e763e80cf28a5ec00c25
Ankit <153371949+CW-Ankit@users.noreply.github.com>
2025-05-26 11:02:50 +0000

    supabse setup

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,15 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "@/utils/supabase/middleware";
+// /middleware.ts
+import { createMiddlewareClient } from '@supabase/ssr';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+export async function middleware(req: NextRequest) {
+    const res = NextResponse.next();
+    const supabase = createMiddlewareClient({ req, res });
+
+    await supabase.auth.getSession(); // optional but recommended
+    return res;
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
-     * Feel free to modify this pattern to include more paths.
-     */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
+    matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 };
