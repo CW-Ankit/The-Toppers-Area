1a61c50f1c5d8a8d44c697585708a400e1ba6fdc
Ankit <153371949+CW-Ankit@users.noreply.github.com>
2025-10-23 09:38:25 +0000

    deployment fixing

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,7 +1,15 @@
-import { createBrowserClient } from "@supabase/ssr";
+// utils/supabase/client.ts
+'use client'
+import { createBrowserClient } from '@supabase/ssr'
 
-export const createClient = () =>
-  createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
+export const createClient = () => {
+    if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+        throw new Error(
+            '@supabase/ssr: Your project URL and API key are required to create a Supabase client!'
+        )
+    }
+    return createBrowserClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    )
+}
